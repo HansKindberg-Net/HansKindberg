720e73ba3a63aabfe8553754974121f93b2d6542
Hans Kindberg <hans.kindberg@gmail.com>
2012-05-02 02:00:04 +0200

    Changed assembly information, again.

--- Properties/SolutionInfo.cs
+++ Properties/SolutionInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 */
 
 [assembly: AssemblyFileVersion("1.0.2012.0501")]
-[assembly: AssemblyInformationalVersion("1.0")]
+//[assembly: AssemblyInformationalVersion("1.0")]
 [assembly: AssemblyVersion("1.0.0.1")]
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
