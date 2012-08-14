45b46c085a6fa4b8bea32e55bbd6ec76fdcf6e6a
Hans Kindberg <hans.kindberg@gmail.com>
2012-08-14 02:56:47 +0200

    Added HansKindberg.Collections.Specialized.Extensions.NameValueCollectionExtension in HansKindberg.Web.

---
+++ HansKindberg.Web.Tests/Collections/Specialized/Extensions/NameValueCollectionExtensionTest.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Web;
+using HansKindberg.Collections.Specialized.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HansKindberg.Web.Tests.Collections.Specialized.Extensions
+{
+	[TestClass]
+	public class NameValueCollectionExtensionTest
+	{
+		#region Methods
+
+		private static void ToQueryStringTest(string expectedQueryString, NameValueCollection nameValueCollection)
+		{
+			Assert.AreEqual(expectedQueryString, nameValueCollection.ToQueryString());
+		}
+
+		[TestMethod]
+		public void ToQueryString_IfTheNameValueCollectionIsEmpty_ShouldReturnAnEmptyString()
+		{
+			Assert.AreEqual(string.Empty, new NameValueCollection().ToQueryString());
+		}
+
+		[TestMethod]
+		public void ToQueryString_IfTheNameValueCollectionIsNotEmpty_ShouldReturnAQueryString()
+		{
+			ToQueryStringTest(string.Empty, new NameValueCollection {{null, null}, {null, null}});
+
+			ToQueryStringTest("?,First,Second", new NameValueCollection {{null, null}, {null, string.Empty}, {null, "First"}, {null, "Second"}});
+
+			ToQueryStringTest("?Test=+First,+Second+,++Third+++", new NameValueCollection {{"Test", " First"}, {"Test", " Second "}, {"Test", "  Third   "}});
+
+			ToQueryStringTest("?FirstName=FirstValue&SecondName=SecondValue&ThirdName=ThirdValue", new NameValueCollection {{"FirstName", "FirstValue"}, {"SecondName", "SecondValue"}, {"ThirdName", "ThirdValue"}});
+
+			ToQueryStringTest("?Test", new NameValueCollection {{null, "Test"}});
+
+			ToQueryStringTest("?FirstValue&=SecondValue", new NameValueCollection {{null, "FirstValue"}, {string.Empty, "SecondValue"}});
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void ToQueryString_IfTheNameValueCollectionIsNull_ShouldThrowAnArgumentNullException()
+		{
+			((NameValueCollection) null).ToQueryString();
+		}
+
+		[TestMethod]
+		public void ToQueryString_PrerequisiteTest()
+		{
+			// ReSharper disable AssignNullToNotNullAttribute
+			const string expectedQuery = "?NameOne=ValueOne&NameTwo=ValueTwo";
+			Assert.AreEqual(expectedQuery, new Uri("http://localhost/Default.html" + expectedQuery).Query);
+
+			NameValueCollection nameValueCollection = new NameValueCollection {{null, null}};
+			Assert.AreEqual(1, nameValueCollection.Count);
+
+			nameValueCollection = new NameValueCollection {{null, null}, {null, null}};
+			Assert.AreEqual(1, nameValueCollection.Count);
+
+			nameValueCollection = new NameValueCollection {{null, null}, {null, string.Empty}, {null, "First"}, {null, "Second"}};
+			Assert.AreEqual(nameValueCollection[null], ",First,Second");
+
+			nameValueCollection = HttpUtility.ParseQueryString("?Test");
+			Assert.AreEqual(1, nameValueCollection.Count);
+			Assert.IsNull(nameValueCollection.Keys[0]);
+			Assert.AreEqual("Test", nameValueCollection[0]);
+			Assert.AreEqual("Test", nameValueCollection[null]);
+
+			nameValueCollection = HttpUtility.ParseQueryString("?Test&&Test");
+			Assert.AreEqual(1, nameValueCollection.Count);
+			Assert.IsNull(nameValueCollection.Keys[0]);
+			Assert.AreEqual("Test,,Test", nameValueCollection[0]);
+			Assert.AreEqual("Test,,Test", nameValueCollection[null]);
+
+			Assert.AreEqual(1, string.Empty.Split(",".ToCharArray(), StringSplitOptions.None).Length);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void ToQueryString_WithOneParameter_IfTheNameValueCollectionIsNotNullAndIfTheEncodingParameterIsNull_ShouldThrowAnArgumentNullException()
+		{
+			new NameValueCollection().ToQueryString(null);
+		}
+
+		#endregion
+	}
+}
---
+++ HansKindberg.Web/Collections/Specialized/Extensions/NameValueCollectionExtension.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Text;
+using System.Web;
+
+// ReSharper disable CheckNamespace
+
+namespace HansKindberg.Collections.Specialized.Extensions
+// ReSharper restore CheckNamespace
+{
+	public static class NameValueCollectionExtension
+	{
+		#region Methods
+
+		public static string ToQueryString(this NameValueCollection nameValueCollection)
+		{
+			return nameValueCollection.ToQueryStringInternal(null);
+		}
+
+		public static string ToQueryString(this NameValueCollection nameValueCollection, Encoding encoding)
+		{
+			if(nameValueCollection == null)
+				throw new ArgumentNullException("nameValueCollection");
+
+			if(encoding == null)
+				throw new ArgumentNullException("encoding");
+
+			return nameValueCollection.ToQueryStringInternal(encoding);
+		}
+
+		private static string ToQueryStringInternal(this NameValueCollection nameValueCollection, Encoding encoding)
+		{
+			if(nameValueCollection == null)
+				throw new ArgumentNullException("nameValueCollection");
+
+			string queryString = string.Empty;
+
+			foreach(string key in nameValueCollection.Keys)
+			{
+				queryString += !string.IsNullOrEmpty(queryString) ? "&" : string.Empty;
+
+				if(key != null)
+					queryString += key + "=";
+
+				// ReSharper disable AssignNullToNotNullAttribute
+				string currentValue = nameValueCollection[key];
+				// ReSharper restore AssignNullToNotNullAttribute
+
+				if(currentValue == null)
+					continue;
+
+				string[] currentValueArray = currentValue.Split(",".ToCharArray(), StringSplitOptions.None);
+				for(int i = 0; i < currentValueArray.Length; i++)
+				{
+					if(i > 0)
+						queryString += ",";
+
+					queryString += encoding == null ? HttpUtility.UrlEncode(currentValueArray[i]) : HttpUtility.UrlEncode(currentValueArray[i], encoding);
+				}
+			}
+
+			return string.IsNullOrEmpty(queryString) ? string.Empty : "?" + queryString;
+		}
+
+		#endregion
+	}
+}
