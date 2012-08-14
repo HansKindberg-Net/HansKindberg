130f7631f14cd81bf0e62ee8e0cd9f9589e1370b
Hans Kindberg <hans.kindberg@gmail.com>
2012-08-14 12:22:40 +0200

    Added HansKindberg.DirectoryServices.ShimTests project.

---
+++ HansKindberg.DirectoryServices.ShimTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("Tests for XXX")]
+[assembly: Guid("885fecec-cc31-4dee-8674-915b8169b28f")]
+// ReSharper disable CheckNamespace
+internal static class AssemblyInfo
+// ReSharper restore CheckNamespace
+{
+	#region Fields
+
+	internal const string AssemblyName = "HansKindberg.DirectoryServices.ShimTests";
+
+	#endregion
+}
