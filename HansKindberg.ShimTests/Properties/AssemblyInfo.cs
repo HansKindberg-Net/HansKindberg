b72ac4dfb355466bd6c18e330ae1bd3378db49f8
Hans Kindberg <hans.kindberg@gmail.com>
2013-01-13 18:42:08 +0100

    Added shimtest projects.

---
+++ HansKindberg.ShimTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("Shimtests for HansKindberg")]
+[assembly: Guid("fc1e9886-a0f2-4f85-9b45-1393f5681a53")]
+// ReSharper disable CheckNamespace
+internal static class AssemblyInfo
+// ReSharper restore CheckNamespace
+{
+	#region Fields
+
+	internal const string AssemblyName = "HansKindberg.ShimTests";
+
+	#endregion
+}
---
+++ HansKindberg.Web.ShimTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("Shimtests for HansKindberg.Web")]
+[assembly: Guid("bd15d10b-e052-41d1-bd69-8ff671b4f746")]
+// ReSharper disable CheckNamespace
+internal static class AssemblyInfo
+// ReSharper restore CheckNamespace
+{
+	#region Fields
+
+	internal const string AssemblyName = "HansKindberg.Web.ShimTests";
+
+	#endregion
+}
