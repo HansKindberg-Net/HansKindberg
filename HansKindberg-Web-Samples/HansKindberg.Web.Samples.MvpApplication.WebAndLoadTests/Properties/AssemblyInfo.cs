386f173cbb924143368ae50c4b215c06784d21c1
Hans Kindberg <hans.kindberg@gmail.com>
2014-02-20 15:07:09 +0100

    Added web and load test project. The reason is to load test the HtmlTransforming functionality.

---
+++ HansKindberg-Web-Samples/HansKindberg.Web.Samples.MvpApplication.WebAndLoadTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("Web and load tests for HansKindberg.Web.Samples.MvpApplication.")]
+[assembly: CLSCompliant(true)]
+[assembly: Guid("7397c0ae-183e-4965-b301-957bd4f1b4b4")]
+// ReSharper disable CheckNamespace
+internal static class AssemblyInfo
+// ReSharper restore CheckNamespace
+{
+	#region Fields
+
+	internal const string AssemblyName = "HansKindberg.Web.Samples.MvpApplication.WebAndLoadTests";
+
+	#endregion
+}
